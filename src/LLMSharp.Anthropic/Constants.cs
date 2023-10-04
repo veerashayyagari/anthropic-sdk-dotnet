770566c9015ef1eb5c82d20b1904fbdbaa4df8c0
Veerash Ayyagari <veerash@veerashayyagari.com>
2023-10-04 16:24:43 -0700

    enable public api documentation to be available through nugets

--- src/LLMSharp.Anthropic/Constants.cs
+++ src/LLMSharp.Anthropic/Constants.cs
@@ -6,11 +6,29 @@
     public class Constants
     {
 #pragma warning disable CA1707 // Identifiers should not contain underscores
+        /// <summary>
+        /// Anthropic Completions API Human Prompt string
+        /// </summary>
         public const string HUMAN_PROMPT = "\n\nHuman:";
+        /// <summary>
+        /// Anthropic Completions API AI Prompt string
+        /// </summary>
         public const string AI_PROMPT = "\n\nAssistant:";
+        /// <summary>
+        /// Anthropic Completions API prompt format
+        /// </summary>
         public const string PROMPT_FORMAT = "\n\nHuman:{0} \n\nAssistant:";
+        /// <summary>
+        /// User agent string for AnthropicClient SDK
+        /// </summary>
         public const string LLMSHARP_USER_AGENT = "llmsharp-anthropic-client-sdk";
+        /// <summary>
+        /// Default Anthropic API endpoint version
+        /// </summary>
         public const string ANTHROPIC_API_VERSION = "2023-06-01";
+        /// <summary>
+        /// Default Anthropic API completions endpoint
+        /// </summary>
         public const string COMPLETIONS_ENDPOINT = "/v1/complete";
 #pragma warning restore CA1707 // Identifiers should not contain underscores
     }
--- src/LLMSharp.Anthropic/Models/AnthropicCompletionError.cs
+++ src/LLMSharp.Anthropic/Models/AnthropicCompletionError.cs
@@ -1,21 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace LLMSharp.Anthropic.Models
 {
+    /// <summary>
+    /// Class representing the error payload from Anthropic API
+    /// </summary>
     public class AnthropicCompletionErrorPayload
     {
+        /// <summary>
+        /// Type of the Anthropic API error
+        /// </summary>
         [JsonPropertyName("type")]
         public string? ErrorType { get; set; }
 
+        /// <summary>
+        /// Error message for the current API failure
+        /// </summary>
         [JsonPropertyName("message")]
         public string? ErrorMessage { get; set; }
     }
 
+    /// <summary>
+    /// Class representing the error response from Anthropic API
+    /// </summary>
     public class AnthropicCompletionError
     {
+        /// <summary>
+        /// Error payload
+        /// </summary>
         [JsonPropertyName("error")]
         public AnthropicCompletionErrorPayload? Error { get; set; }
     }
